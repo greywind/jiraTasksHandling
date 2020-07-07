4d6e3704ec3339b9403f238fa7101d558ea36520
Sergey Brezitskiy <sergey.brezitskiy@gmail.com>
2020-07-07 16:08:27 +0300

    show spinner while list of tasks is loading

--- ui/src/components/home/index.tsx
+++ ui/src/components/home/index.tsx
@@ -4,18 +4,24 @@ import { whyDidYouRender } from "@core/utils";
 import useStyles from "./styles";
 import { TasksSvcContext } from "@services/servicesProvider";
 import { Issue } from "src/models/task";
+import { Spinner } from "reactstrap";
 
 const Home: React.FunctionComponent<Props> = () => {
     const classes = useStyles();
     const { getAllIssuesInTheCurrentSprint } = useContext(TasksSvcContext);
     const [tasks, setTasks] = useState<Issue[]>([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         (async () => {
             setTasks(await getAllIssuesInTheCurrentSprint());
+            setLoading(false);
         })();
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
+
+    if (loading)
+        return <Spinner className={classes.spinner} />;
 
     return <div className={classes.divClass}>
         Hello, world!
--- ui/src/components/home/styles.ts
+++ ui/src/components/home/styles.ts
@@ -1,11 +1,20 @@
 import { createUseStyles } from "react-jss";
 import { Theme } from "src/theme/jss";
 
+const spinnerSize = 100;
+
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 const styles = (theme: Theme) => ({
     divClass: {
         fontSize: 62,
         fontWeight: 200,
+    },
+    spinner: {
+        position: "fixed",
+        height: spinnerSize,
+        width: spinnerSize,
+        top: `calc(45vh - ${spinnerSize / 2}px)`,
+        left: `calc(50vw - ${spinnerSize / 2}px)`,
     },
 });
 type Names = keyof ReturnType<typeof styles>;
